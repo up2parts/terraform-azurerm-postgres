0960ff28e073ec6880c976d785a27bce6c2b15ff
Sebastian Sterk <7263970+sebastiansterk@users.noreply.github.com>
2023-01-19 10:52:11 +0100

    fix: indentation

--- configuration.tf
+++ configuration.tf
@@ -1,7 +1,7 @@
 resource "azurerm_postgresql_configuration" "this" {
   for_each = {for config in var.postgresql_configuration: config.name => config}
-  name                = "${each.value.name}"
+	name                = "${each.value.name}"
 	server_name         = azurerm_postgresql_server.this.name
-  resource_group_name = var.resource_group_name
+	resource_group_name = var.resource_group_name
 	value               = "${each.value.value}"
   }
